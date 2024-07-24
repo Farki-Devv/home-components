e7ed3809d8401b1effdb95d002c21e3079d1dd6c
Farrux <ganiboyevfarrux468@gmail.com>
2024-07-23 23:52:37 -0700

    changed components

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,34 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import React from 'react'
 import './index.css'
-
-import { Checkbox } from './components/TF-ui/checkbox'
-
-import { Calendar } from './components/TF-ui/calendar'
-
-import { DataTable } from './components/TF-ui/data-table'
+// breadcrumb component
 import { Breadcrumb, BreadcrumbEllipsis, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from './components/TF-ui/breadcrumb'
-import { IconChevronDown } from '@tabler/icons-react'
+
+// card component
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from './components/TF-ui/card'
-import { Input } from './components/TF-ui/input'
-import { Button } from './components/TF-ui/button'
+
+// carousel component
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from './components/TF-ui/carousel'
 
-// import { Button } from './components/ui/button'
+// calendar component
+import { Calendar } from './components/TF-ui/calendar'
+
+
+// data table component
+import { DataTable } from './components/TF-ui/data-table'
+
+// button component
+import { Button } from './components/TF-ui/button'
+
+// checbox component
+import { Checkbox } from './components/TF-ui/checkbox'
+
+// icons
+import { IconChevronDown, IconCircle } from '@tabler/icons-react'
+
+// input component, but I haven't changed the input component of this shadcn ui yet
+import { Input } from './components/TF-ui/input'
+
 function App() {
   React.useEffect(() => {
     (CSS as any).paintWorklet.addModule("squircle.min.js");
@@ -103,34 +117,6 @@ function App() {
 </div>
 
 <div className='ml-14'>
-<Carousel className="w-full max-w-[370px]">
-      <CarouselContent>
-        {Array.from({ length: 2 }).map((_, index) => (
-          <CarouselItem key={index} >
-            <div className="p-1">
-              <Card className='w-[360px]'>
-                <CardContent className="flex aspect-square items-center justify-center p-6">
-              
-                </CardContent>
-              </Card>
-            </div>
-          </CarouselItem>
-        ))}
-      </CarouselContent>
-      <CarouselPrevious />
-      <CarouselNext />
-    </Carousel>
-</div>
-<div className='ml-2 mt-10 w-[900px]'>
-{/* <Calendar/> */}
-
-
-
-
-{/* <Checkbox/> */}
-<div className='ml-20 '>
-
-
 {/* <Carousel className="w-full max-w-[370px]">
       <CarouselContent>
         {Array.from({ length: 2 }).map((_, index) => (
@@ -148,16 +134,77 @@ function App() {
       <CarouselPrevious />
       <CarouselNext />
     </Carousel> */}
-
-  
-    <DataTable/>
-    <Checkbox/>
-    </div>
-    <Calendar/>
-    {/* <DemoCalendar /> */}
-    {/* <Button className='w-[60px] text-white'>Done</Button> */}
-
 </div>
+
+
+    <div className='ml-2 mt-10 w-[900px]'>
+        {/* calendar component */}
+        {/* <Calendar/> */}
+    </div>
+
+
+    <div className='ml-10 mt-10'>
+      {/* checkbox component */}
+      {/* <Checkbox/> */}
+    </div>
+
+
+     <div className='ml-20 '>
+    {/* Data table component */}
+    {/* <DataTable/> */}
+    </div>
+
+
+
+
+
+
+
+
+
+
+    {/* Button component */}
+     <div className='flex flex-col space-y-2'>
+      {/* Button component */}
+
+      {/* size lg */}
+      {/* <div className='flex gap-2 px-3'>
+      <Button size={'lg'} variant={'default'}><IconCircle className='size-5'/>Button <IconCircle className='size-5'/></Button>
+      <Button size={'lg'} variant={'secondary'}><IconCircle className='size-5'/>Button <IconCircle className='size-5'/></Button> <Button size={'lg'} variant={'destructive'}><IconCircle className='size-5'/>Button <IconCircle className='size-5'/></Button> <Button size={'lg'} variant={'outline'}><IconCircle className='size-5'/>Button <IconCircle className='size-5'/></Button> <Button size={'lg'} variant={'ghost'}><IconCircle className='size-5'/>Button <IconCircle className='size-5'/></Button>
+      </div> */}
+
+     {/* size default */}
+     {/* <div className='flex gap-8'>
+      <Button variant={'default'}><IconCircle className='size-4'/>Button<IconCircle className='size-4'/></Button>
+      <Button variant={'secondary'}><IconCircle/>Button<IconCircle/></Button>
+      <Button variant={'destructive'}><IconCircle/>Button<IconCircle/></Button>
+      <Button variant={'outline'}><IconCircle/>Button<IconCircle/></Button>
+      <Button variant={'ghost'}><IconCircle/>Button<IconCircle/></Button>
+     </div> */}
+
+    {/* size sm */}
+     {/* <div className='flex gap-11'>
+      <Button size={'sm'} variant={'default'}><IconCircle/>Button <IconCircle/></Button>
+      <Button size={'sm'} variant={'secondary'}><IconCircle/>Button<IconCircle/></Button>
+      <Button size={'sm'} variant={'destructive'}><IconCircle/>Button<IconCircle/></Button>
+      <Button size={'sm'} variant={'outline'}><IconCircle/>Button<IconCircle/></Button>
+      <Button size={'sm'} variant={'ghost'}><IconCircle/>Button<IconCircle/></Button>
+     </div> */}
+
+
+     {/* size icon */}
+
+     {/* <div className='flex gap-24'>
+      <Button size={'icon'} variant={'default'}><IconCircle className='size-5'/></Button>
+      <Button size={'icon'} variant={'secondary'}><IconCircle className='size-5'/></Button>
+      <Button size={'icon'} variant={'destructive'}><IconCircle className='size-5'/></Button>
+      <Button size={'icon'} variant={'outline'}><IconCircle className='size-5'/></Button>
+      <Button size={'icon'} variant={'ghost'}><IconCircle className='size-5'/></Button>
+     </div> */}
+     
+     </div>
+ 
+
 </div>
 
 
--- src/components/TF-ui/button.tsx
+++ src/components/TF-ui/button.tsx
@@ -1,30 +1,45 @@
 /**
+ * @file button.tsx Button component
+ * @overview Displays a button or a component that looks like a button.
+ * @example
+ * ```tsx
+ * import { Button } from './components/TF-ui/button'
  * 
- * Tayyor emas ! Tayyor emas ! Tayyor emas ! Tayyor emas !
+ * <Button>Button</Button>
+ * ```
+ * @require "@radix-ui/react-slot": "^1.1.0" - npm i @radix-ui/react-slot@1.1.0
+ * @require "class-variance-authority": "^0.7.0", - npm i class-variance-authority@0.7.0
  */
 
 import { Slot } from '@radix-ui/react-slot'
 import { cva, type VariantProps } from 'class-variance-authority'
 import * as React from 'react'
-
 import { cn } from '@/lib/utils'
 
-
-const buttonVariants = cva("inline-flex items-center justify-center rounded-lg text-sm font-medium gap-2 font-inter font-medium h-9  disabled:pointer-events-none  disabled:opacity-50 font-inter  cursor-pointer", 
+/**
+ * Button variants and sizes for button component
+ * @example
+ * ```tsx
+ * import { Button } from './components/TF-ui/button'
+ * 
+ * <Button variant={'destructive'} size={'lg'}>Button</Button>
+ * ```
+ */
+const buttonVariants = cva("inline-flex items-center justify-center rounded-lg text-sm font-medium gap-2 font-inter font-medium h-9  disabled:pointer-events-none disabled:opacity-50 font-inter  cursor-pointer", 
 	{
 		variants:{
 			variant:{
-				default:"bg-blue-800 text-white hover:bg-blue-700 focus:outline-none focus:ring focus:ring-blue-800-16 active:bg-blue-900 disabled:bg-blue-800  text-white focur:bg-blue-800-24 ",
+				default:"bg-blue-800 text-white hover:bg-blue-700 focus:outline-none focus:ring focus:ring-blue-800-16 active:bg-blue-900 disabled:bg-blue-800  text-white focur:bg-blue-800-24 squircle-default",
 				secondary:"bg-neutral-100 text-neutral-950 hover:bg-neutral-50 focus:ring focus:ring-blue-800-24  active:bg-neutral-100 disabled:bg-neutral-100 squircle-secondary",
 				destructive:"bg-red-800 text-white hover:bg-red-700 focus:outline-none focus:ring focus:ring-red-800-16 active:bg-red-900 disabled:bg-red-800 squircle-destructive-btn",
 				outline:"bg-white hover:bg-neutral-50 focus:outline-none focus:ring   text-neutral-950 focus:ring-blue-800-16 active:bg-neutral-100 squircle",
 				ghost:"hover:bg-neutral-50 hover:text-accent-foreground active:bg-neutral-0",
 			},
 			size:{
-				default:"h-9",
-				sm:"h-8 rounded-md px-3",
-				lg:"h-11 rounded-md px-8",
-				icon:"h-10 w-10",
+				lg:"h-10 w-33 px-4 py-2 text-base",
+				default:"h-9 w-30 px-3 py-2 text-sm",
+				sm:"h-8 w-23 px-2 py-2 text-xs",
+				icon:"size-10",
 			},
 		},	
 		defaultVariants:{
@@ -33,11 +48,31 @@ const buttonVariants = cva("inline-flex items-center justify-center rounded-lg t
 		}
 	}
 )
-
+/**
+ * @interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement>
+ * @property {string} className Optional additional class names for custom styles
+ * @property {boolean} asChild asChild to use a component within a component
+ */
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement>, VariantProps<typeof buttonVariants>{
 	asChild?:boolean
+	className?:string
 }
-
+/**
+ * Button component
+ * Displays a button or a component that looks like a button.
+ * @param {ButtonProps} props The properties for the Button component
+ * @param {string} className Optional additional class names for custom styles
+ * @param {object} props to receive props
+ * @param variant - The button variants
+ * @param size - The button sizes
+ * @returns {JSX.Element} Returns the Button component
+ * @example
+ * ```tsx
+ * import { Button } from './components/TF-ui/button'
+ * 
+ * <Button variant={'destructive'} size={'lg'}>Button</Button>
+ * ```
+ */
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(({className, variant, size, asChild = false ,...props}, ref)=>{
 	const Comp = asChild ? Slot : 'button'
 	return(
@@ -48,5 +83,5 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(({className, var
 	)
 })
 Button.displayName = 'Button'
-
-export { Button, buttonVariants }
+export type {ButtonProps}
+export {buttonVariants,  Button }
